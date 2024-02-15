1e095c32a6d10ce1e7561196ce7e1bd51d4d5d31
vetri <vetrichelvi26@gmail.com>
2024-02-15 14:41:53 +0530

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
+
+class PostController extends Controller
+{
+    public function index(){
+        $users = \App\Models\User::all();
+        return $users;
+
+    }
+
+    public function create(){
+        $newUser = new \App\Models\User;
+        $newUser->name = 'John Doe';
+        $newUser->email = 'john@example.com';
+        $newUser->save();
+        if($newUser){
+            return 'data successfully inserted';
+        }
+    }
+
+
+    public function update(){
+        $userToUpdate = \App\Models\User::find(2);
+
+        // Update user details
+        $userToUpdate->name = 'john';
+        $userToUpdate->save();
+        if($userToUpdate){
+            return 'data successfully Updated';
+        }
+    }
+
+    public function delete(){
+        $userToDelete = \App\Models\User::find(2);
+        $userToDelete->delete();
+        if($userToDelete){
+            return 'data successfully delete';
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/user', [PostController::class, 'index']);
+Route::get('/usercreate', [PostController::class, 'create']);
+Route::get('/userupdate', [PostController::class, 'update']);
+Route::get('/userdelete', [PostController::class, 'delete']);
+
+
+
